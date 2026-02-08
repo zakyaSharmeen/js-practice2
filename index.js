03b54fceb8207421693fc2ae426eb00795f3dcdd
zakyaSharmeen <zakyasharmeen@gmail.com>
2026-02-08 23:31:23 +0530

    added

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("Hello, World!");
+// This is a simple JavaScript file that prints "Hello, World!" to the console. You can run this code in a JavaScript environment, such as a web browser's developer console or Node.js.
